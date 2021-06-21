200dfdf1d217b8a666ca09315bc6cec3c1aff9b7
Vadim <vtimofeev223@gmail.com>
2021-06-21 13:14:59 +0300

    Added Master Password Settings

---
+++ vue/components/MasterPasswordAdminSettings.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-scroll-area class="full-height full-width">
+    <div class="q-pa-lg ">
+      <div class="row q-mb-md">
+        <div class="col text-h5" v-t="'MAILMASTERPASSWORD.HEADING_SETTINGS_TAB'"></div>
+      </div>
+      <q-card flat bordered class="card-edit-settings">
+        <q-card-section>
+          <div class="row q-mb-md">
+            <div class="col-2 q-mt-sm" v-t="'MAILMASTERPASSWORD.LABEL_MASTER_PASSWORD'"></div>
+            <div class="col-4">
+              <q-input outlined dense class="bg-white"  ref="oldPassword" type="password" v-model="password"/>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+      <div class="q-pa-md text-right">
+        <q-btn unelevated no-caps dense class="q-px-sm" :ripple="false" color="primary"
+               :label="saving ? $t('COREWEBCLIENT.ACTION_SAVE_IN_PROGRESS') : $t('COREWEBCLIENT.ACTION_SAVE')"
+               @click="save"/>
+      </div>
+    </div>
+    <UnsavedChangesDialog ref="unsavedChangesDialog"/>
+  </q-scroll-area>
+</template>
+
+<script>
+import UnsavedChangesDialog from 'src/components/UnsavedChangesDialog'
+import webApi from 'src/utils/web-api'
+import notification from 'src/utils/notification'
+import errors from 'src/utils/errors'
+import _ from "lodash";
+import settings from "../../../BrandingWebclient/vue/settings";
+
+export default {
+  name: 'MasterPasswordAdminSettings',
+  components: {
+    UnsavedChangesDialog
+  },
+  data () {
+    return {
+      saving: false,
+      password: '',
+      fakePass: '     '
+    }
+  },
+  mounted() {
+    this.password = this.fakePass
+  },
+  beforeRouteLeave (to, from, next) {
+    if (this.hasChanges() && _.isFunction(this?.$refs?.unsavedChangesDialog?.openConfirmDiscardChangesDialog)) {
+      this.$refs.unsavedChangesDialog.openConfirmDiscardChangesDialog(next)
+    } else {
+      next()
+    }
+  },
+  methods: {
+    hasChanges () {
+      return this.password !== this.fakePass
+    },
+    save () {
+      if (!this.saving) {
+        this.saving = true
+        const parameters = {
+          MasterPassword: this.password,
+        }
+        webApi.sendRequest({
+          moduleName: 'MailMasterPassword',
+          methodName: 'UpdateSettings',
+          parameters,
+        }).then(result => {
+          this.fakePass = this.password
+          this.saving = false
+          if (result === true) {
+            notification.showReport(this.$t('COREWEBCLIENT.REPORT_SETTINGS_UPDATE_SUCCESS'))
+          } else {
+            notification.showError(this.$t('COREWEBCLIENT.ERROR_SAVING_SETTINGS_FAILED'))
+          }
+        }, response => {
+          this.saving = false
+          notification.showError(errors.getTextFromResponse(response, this.$t('COREWEBCLIENT.ERROR_SAVING_SETTINGS_FAILED')))
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
