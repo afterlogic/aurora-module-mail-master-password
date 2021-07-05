23f3414c584a58f903470cb4c6d41d55d5e991aa
nadine-shi <nadinshi@gmail.com>
2021-07-05 20:43:46 +0300

    fixed issue with paths

--- vue/manager.js
+++ vue/manager.js
@@ -9,7 +9,7 @@ export default {
         tabName: 'mailmasterpassword',
         title: 'MAILMASTERPASSWORD.LABEL_SETTINGS_TAB',
         component () {
-          return import('src/../../../MailMasterPassword/vue/components/MasterPasswordAdminSettings')
+          return import('./components/MasterPasswordAdminSettings')
         },
       },
     ]
